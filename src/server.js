6e06facc1efdabc84ae408d6bc3182211be5985b
highway92 <highway@highway92ui-MacBookAir.local>
2021-08-21 20:11:01 +0900

    message stringify for nickname

--- src/server.js
+++ src/server.js
@@ -15,4 +15,16 @@ const handleListen = () => console.log("Listening on http://localhost:3000🐵")
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser 👍");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
+  });
+});
+
 server.listen(3000, handleListen);
